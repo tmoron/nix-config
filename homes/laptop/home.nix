49fbb96f88074840b3294f631d64ce20e9de8c9c
tomoron <tomoron@student.42angouleme.fr>
2024-10-18 17:59:17 +0200

    fix laptop config

--- homes/laptop/home.nix
+++ homes/laptop/home.nix
@@ -3,13 +3,12 @@
 {
 	imports = [
 	  ../modules/game.nix
-	  ./bspwm.nix
 	];
 
     programs.alacritty.settings.font.size = 8;
 
 	home.file = {
       ".config/bspwm/host.sh".source = ./bspwm/host.sh;
-	  ".config/bspwm/screenlayout.sh".source = ./bspwm/host.sh;
-	}
+#	  ".config/bspwm/screenlayout.sh".source = ./bspwm/host.sh;
+	};
 }
