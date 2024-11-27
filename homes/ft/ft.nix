d8de37b038c5efaa110dad1e8d411ddff2ef9d34
tomoron <tomoron@student.42angouleme.fr>
2024-11-27 17:32:16 +0100

    disable firefox profile on profile ft and add brightnessctl to ft home packages

--- homes/ft/ft.nix
+++ homes/ft/ft.nix
@@ -6,10 +6,12 @@
   home.packages = with pkgs;[	
 	neovim
 	dmenu
+	brightnessctl
   ];
 
   home.file = {
     ".config/sxhkd".source = dotfiles/config/sxhkd;
 	".config/bspwm/host.sh".source = ./bspwm/host.sh;
   };
+  programs.firefox.enable = false;
 }
--- modules/firefox.nix
+++ modules/firefox.nix
@@ -2,7 +2,7 @@
 
 {
   programs.firefox = {
-    enable = true;
+    enable = lib.mkDefault true;
     profiles.default = {
       extensions = with inputs.firefox-addons.packages."x86_64-linux"; 
 	  [vimium ublock-origin];
