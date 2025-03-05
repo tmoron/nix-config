a6cd1d46590f3d1effa0a4c13288581a5f0651a8
tomoron <tomoron@student.42angouleme.fr>
2025-03-05 20:53:15 +0100

    repair sync command module

--- homeConfigs/modules/sync.nix
+++ homeConfigs/modules/sync.nix
@@ -1,17 +1,31 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, ... }:
 
 {
-/*
-  options.mods.sync.enable = lib.mkOption {
-    type = lib.types.bool;
-	default = true;
-	description = "enable desk_sync command";
+  options.mods.sync = {
+	enable = lib.mkOption {
+      type = lib.types.bool;
+	  default = true;
+	  description = "enable usr_sync command";
+	};
+
+	homeFolder = lib.mkOption {
+		type = lib.types.str;
+		default = "/home/tom/";
+		description = "folder that will be synced with the server";
+	};
+
+	destFolder = lib.mkOption {
+		type = lib.types.str;
+		default = "/raid/pc_sync";
+		description = "where on the server";
+	};
   };
 
-  home.file.".local/bin/desk_sync" = pkgs.writeShellScriptBin "desk_sync" ''
-  ${pkgs.unison}/
-  config = lib.mkIf config.mods.sync.enable {
-	'';
+  config.home.file.".local/bin/usr_sync" =  lib.mkIf config.mods.sync.enable {
+  	text = ''
+	#!/bin/env
+	unison "${config.mods.sync.homeFolder}" "ssh://tom@tmoron.fr:1880/${config.mods.sync.destFolder}" $@
+  	'';
+	executable = true;
   };
-  */
 }
