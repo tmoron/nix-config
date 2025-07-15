27ca1796970ac53d4940e804cab53afafdfd3137
tomoron <tomoron@student.42angouleme.fr>
2025-07-15 16:12:16 +0200

    update flake, sort system and home packages

--- homeConfigs/packages.nix
+++ homeConfigs/packages.nix
@@ -6,7 +6,7 @@
 #    By: tomoron <tomoron@student.42angouleme.fr>   +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/02/09 22:01:56 by tomoron           #+#    #+#              #
-#    Updated: 2025/04/15 15:58:07 by tomoron          ###   ########.fr        #
+#    Updated: 2025/07/15 16:10:34 by tomoron          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,22 +14,34 @@
 
 {
   home.packages = with pkgs;[
-	ripgrep
-
-	activate-linux
-	unison
-	brightnessctl	
-	playerctl
-	python3
-	vlc
-    nerd-fonts.iosevka
-
-	nix-index
-	yubikey-manager
-	yubico-pam
-	dunst
-	bibata-cursors
-	libcaca
-	nasm
+    lrzip
+    pigz
+    htop
+    gnumake
+    git
+    neofetch
+    neovim
+    clang
+    ninja
+    gdb
+    valgrind
+    wget
+    cmake
+    man-pages
+    stress
+    ffmpeg-full
+    ripgrep
+    activate-linux
+    unison
+    brightnessctl  
+    playerctl
+    python3
+    vlc
+    nix-index
+    dunst
+    yubikey-manager
+    bibata-cursors
+    libcaca
+    nasm
   ];
 }
--- osConfigs/packages.nix
+++ osConfigs/packages.nix
@@ -2,31 +2,14 @@
 
 {
   environment.systemPackages = with pkgs; [
-    lrzip
-	home-manager
-   	vim
-	pciutils
-	pigz
-	htop
-	gnumake
-	git
-	neofetch
-	neovim
-	ntfs3g
-	clang
-	ninja
-	gdb
-	valgrind
-	wget
-	cmake
-	usbutils
-	man-pages
-	stress
-	cryptsetup
-	acpi
-	dconf
-
-	ffmpeg-full
-	nv-codec-headers
+    home-manager
+    vim
+    pciutils
+    usbutils
+    ntfs3g
+    cryptsetup
+    acpi
+    nerd-fonts.iosevka
+    yubico-pam
   ];
 }
