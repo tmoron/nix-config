c766b089738abb9d86e77d1705f3ba809d4234a7
tomoron <tomoron@student.42angouleme.fr>
2025-07-16 20:07:12 +0200

    remove full home config from iso

--- osConfigs/hosts/iso.nix
+++ osConfigs/hosts/iso.nix
@@ -13,13 +13,13 @@
   
   services.getty.autologinUser = lib.mkForce "tom";
   services.getty.helpLine = lib.mkForce "";
-  programs.hyprland.enable = true;
+#  programs.hyprland.enable = true;
 
-  home-manager.extraSpecialArgs = { inherit inputs; inherit pkgs; isOs = true;};
-  home-manager.users.tom = {
-    imports = [
-	  ../../homeConfigs/home.nix 
-	  ../../homeConfigs/hosts/iso.nix
-	];
-  };
+#  home-manager.extraSpecialArgs = { inherit inputs; inherit pkgs; isOs = true;};
+#  home-manager.users.tom = {
+#    imports = [
+#	  ../../homeConfigs/home.nix 
+#	  ../../homeConfigs/hosts/iso.nix
+#	];
+#  };
 }
