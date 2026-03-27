6913e58e0b4d9a6a227d00b3da7481af7e8bce59
tomoron <tomoron@student.42angouleme.fr>
2026-03-27 12:22:19 +0100

    fix hyprland config

--- homeConfigs/modules/wayland/hyprland.nix
+++ homeConfigs/modules/wayland/hyprland.nix
@@ -96,12 +96,12 @@
 		"$mainMod, o, exec, playerctl play-pause"
       ];
   
-      windowrulev2 = [
-        #ignore maximize requests
-        "suppressevent maximize, class:.*"
-        #fix dragging issues with xwayland
-        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-      ];
+#      windowrulev2 = [
+#        #ignore maximize requests
+#        "suppressevent maximize, class:.*"
+#        #fix dragging issues with xwayland
+#        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+#      ];
   
       input = {
         kb_layout = "fr";
