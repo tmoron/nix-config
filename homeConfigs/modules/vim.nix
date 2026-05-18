3793d9d4c363b3484913790d39b4e0abdcb5bee2
tomoron <tomoron@student.42angouleme.fr>
2026-05-18 17:51:34 +0200

    add 99 nvim to neovim config

--- homeConfigs/modules/vim.nix
+++ homeConfigs/modules/vim.nix
@@ -23,6 +23,7 @@
       nixd
       nodejs_24
       glsl_analyzer
+	  opencode
       (pkgs.python3.withPackages (ps: with ps; [
         python-lsp-server
         pylsp-mypy
@@ -34,6 +35,7 @@
 #    programs.neovim.extraLuaConfig = ''
 #      vim.lsp.config('pylsp', {})
 #    '';
+
 
     programs.neovim.initLua= ''
 	local cmp = require'cmp'
@@ -135,11 +137,54 @@
       })
 
 
+			local _99 = require("99")
 
+            local cwd = vim.uv.cwd()
+            local basename = vim.fs.basename(cwd)
+			_99.setup({
+                provider = _99.Providers.OpenCodeProvider,
+				model = "gemma4:26b",
+				logger = {
+					level = _99.DEBUG,
+					path = "/home/tom/99logs/" .. basename .. ".99.debug",
+					print_on_error = true,
+				},
+                tmp_dir = "./tmp",
 
+                --- Completions: #rules and @files in the prompt buffer
+                completion = {
+                    --- Configure @file completion (all fields optional, sensible defaults)
+                    files = {
+                        -- enabled = true,
+                        -- max_file_size = 102400,     -- bytes, skip files larger than this
+                        -- max_files = 5000,            -- cap on total discovered files
+                        -- exclude = { ".env", ".env.*", "node_modules", ".git", ... },
+                    },
+                    --- File Discovery:
 
+                    source = "cmp", -- "native" (default), "cmp", or "blink"
+                },
 
+				md_files = {
+					-- "AGENT.md",
+				},
+			})
 
+			vim.keymap.set("v", "<leader>9v", function()
+				_99.visual()
+			end)
+
+			vim.keymap.set("n", "<leader>9x", function()
+				_99.stop_all_requests()
+			end)
+
+			vim.keymap.set("n", "<leader>9s", function()
+				_99.search()
+			end)
+
+			vim.keymap.set("n", "<leader>9m", function()
+  				require("99.extensions.telescope").select_model()
+			end)
     '';
 
     programs.neovim.extraConfig = ''
