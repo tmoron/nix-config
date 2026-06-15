3710d953abdace4ae45d70dc5dd77f3e80577466
tomoron <tomoron@student.42angouleme.fr>
2026-06-15 02:28:55 +0200

    fix windowsrulev2 deprecated, fix pinentry unfocused

--- homeConfigs/global/wayland/hyprland.nix
+++ homeConfigs/global/wayland/hyprland.nix
@@ -93,12 +93,9 @@
   	"$mainMod, o, exec, playerctl play-pause"
     ];
 
-     windowrulev2 = [
-       #ignore maximize requests
-       "suppressevent maximize, class:.*"
-       #fix dragging issues with xwayland
-       "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-     ];
+    windowrule = [
+	  "match:class (pinentry-)(.*), stay_focused on"
+	];
 
     input = {
       kb_layout = "fr";
