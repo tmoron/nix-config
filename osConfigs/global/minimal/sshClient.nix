78f43ad41d91520315941295a5798bf6e14bb3ad
tomoron <tomoron@student.42angouleme.fr>
2026-08-16 00:50:26 +0200

    forward gpg agent socket to ssh

--- osConfigs/global/minimal/sshClient.nix
+++ osConfigs/global/minimal/sshClient.nix
@@ -2,6 +2,7 @@
     programs.ssh.extraConfig = ''
         Host tmoron.fr
           HostName tmoron.fr
+          RemoteForward /run/user/1000/gnupg/S.gpg-agent /run/user/1000/gnupg/S.gpg-agent
           Port 1880 
     '';
 }
